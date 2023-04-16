90e5c98c3f404f913e5639ed2a75101d484f6d23
Chip <cstep772@gmail.com>
2023-04-16 17:24:52 -0400

    Added the computer choice function

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,1 +1,14 @@
 console.log("lets play!")
+
+const choices = ["rock", "paper", "scissors"];
+
+const getComputerChoice = () => {
+    // get a number between 0 and 3
+    const random = Math.floor(Math.random() * choices.length);
+    console.log(random, choices[random]);
+    // return the index of choices
+    return choices[random];
+}
+
+// call the computer choice function
+getComputerChoice()
