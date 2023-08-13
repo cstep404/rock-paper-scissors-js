e885aea1a77ee3837b9c3c86ad932f92eccf3c36
Chip <cstep772@gmail.com>
2023-08-13 14:35:51 -0400

    Added score tracker and number of games tracker

--- scripts/rps.js
+++ scripts/rps.js
@@ -3,6 +3,11 @@ console.log("LET'S PLAY!!!")
 // main game function
 const game = () => {
 
+    // vars
+    let numberOfGames = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+
     // selectors
     const buttonSelector = document.querySelectorAll('.btn');
     // selects the div named game
@@ -19,12 +24,14 @@ const game = () => {
     }
 
     const startGame = (e) => {
-        console.log(e.target);
+
+        if (numberOfGames < 5) {
+            console.log(e.target);
             let playerSelection = e.target.id;
             let cpuSelection = getComputerChoice();
-
+    
             // create the element, manipulate the element, and then place it on page by appending
-
+    
             // div - results box
             const resultsBox = document.createElement('div');
             resultsBox.textContent = 'Results!'
@@ -33,14 +40,14 @@ const game = () => {
             const playerChoice = document.createElement('p');
             playerChoice.textContent = `Player's selection: ${playerSelection}`;
             resultsBox.appendChild(playerChoice);
-
+    
             // cpu choice
             const cpuChoice = document.createElement('p');
             cpuChoice.textContent = `Computers selection: ${cpuSelection}`;
             resultsBox.appendChild(cpuChoice);
-
+    
             gameDivSelector.appendChild(resultsBox);
-
+    
             // ties
             if (playerSelection === 'rock' && cpuSelection === 'rock') {
                 console.log('Tie');
@@ -54,28 +61,49 @@ const game = () => {
             // player wins
             if (playerSelection === 'rock' && cpuSelection === 'scissors') {
                 console.log('Player wins');
+                playerScore += 1;
             }
             if (playerSelection === 'paper' && cpuSelection === 'rock') {
                 console.log('Player wins');
+                playerScore += 1;
             }
             if (playerSelection === 'scissors' && cpuSelection === 'paper') {
                 console.log('Player wins');
+                playerScore += 1;
             }
             // cpu wins
             if (playerSelection === 'rock' && cpuSelection === 'paper') {
                 console.log('Computer wins');
+                computerScore += 1;
             }
             if (playerSelection === 'paper' && cpuSelection === 'scissors') {
                 console.log('Computer wins');
+                computerScore += 1;
             }
             if (playerSelection === 'scissors' && cpuSelection === 'rock') {
                 console.log('Computer wins');
+                computerScore += 1;
             }
+            numberOfGames += 1;
+            console.log(`number of games: ${numberOfGames}`);
+            console.log(`playerScore: ${playerScore}`);
+            console.log(`computerScore: ${computerScore}`)
+        }
     }
 
     buttonSelector.forEach((button) => {
         button.addEventListener('click', startGame);
     });
+
+    if (numberOfGames === 5) {
+        if (playerScore > computerScore) {
+            console.log(`player wins the game`);
+        } else if (playerScore < computerScore) {
+            console.log(`computer wins the game`);
+        } else {
+            console.log(`tie!`);
+        }
+    }
 }
 
 game();
